86f7113c8c8bf91415de99c92e4c43c87ac9ea63
Joe Morano <joemorano@yahoo.com>
2020-05-16 14:16:43 -0700

    Initial commit round 2

---
+++ src/VariablesContinued/Main.java
@@ -0,0 +1,22 @@
+package VariablesContinued;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "OIJIOJIOJOIJIOJFOESIJOFJeoiwjfoisejoiwe";
+        System.out.println("String length: " + name.length());
+        System.out.println("Forward: ");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.print("Char: " + name.charAt(i) + " ");
+        }
+        System.out.println("\nReverse:");
+        for (int i = name.length() - 1; i > 0 ; i--) {
+            System.out.print("Char: " + name.charAt(i) + " ");
+        }
+
+
+
+
+    }
+
+}
